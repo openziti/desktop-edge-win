ad36b54f214ca909f8ec2f90de3f9f84a3c7d504
ziti-ci <ziti-ci@netfoundry.io>
2020-05-18 00:47:35 -0400

    updates to build.bat to pull deps local to checkout

--- service/build.bat
+++ service/build.bat
@@ -1,9 +1,13 @@
+set SVC_ROOT_DIR=%~dp0
+
+git clone https://github.com/netfoundry/ziti-tunneler-sdk-c.git
+cd ziti-tunneler-sdk-c
+git checkout update-submodule-to-https-vs-git
+
 @echo Updating submodules...
 git submodule update --init --recursive
 
-set SVC_ROOT_DIR=%~dp0
-
-SET TUNNELER_SDK_DIR=%SVC_ROOT_DIR%deps\ziti-tunneler-sdk-c
+SET TUNNELER_SDK_DIR=deps\ziti-tunneler-sdk-c
 set CGO_CFLAGS=-DNOGDI -I %TUNNELER_SDK_DIR%\install\include
 set CGO_LDFLAGS=-L %TUNNELER_SDK_DIR%\install\lib
 
@@ -13,8 +17,8 @@ if not exist %TUNNELER_SDK_DIR%\install mkdir %TUNNELER_SDK_DIR%\install
 cmake -G Ninja -S %TUNNELER_SDK_DIR% -B %TUNNELER_SDK_DIR%\build -DCMAKE_INSTALL_PREFIX=%TUNNELER_SDK_DIR%\install
 cmake --build %TUNNELER_SDK_DIR%\build --target install
 
-cp %TUNNELER_SDK_DIR%\install\lib\ziti.dll .
-cp %TUNNELER_SDK_DIR%\install\lib\libuv.dll .
+cp %TUNNELER_SDK_DIR%\install\lib\ziti.dll %SVC_ROOT_DIR%
+cp %TUNNELER_SDK_DIR%\install\lib\libuv.dll %SVC_ROOT_DIR%
 
 echo COPIED dlls to .
 
