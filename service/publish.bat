99dffa0d32edc079070d71de1348a31c2897cc74
ziti-ci <ziti-ci@netfoundry.io>
2020-05-22 21:45:03 -0400

    add the publish script and comment out the source import

--- service/publish.bat
+++ service/publish.bat
@@ -1,11 +1,11 @@
 SET ZITT_TUNNEL_WIN_VER=0.0.4
 IF "%TRAVIS_BRANCH%"=="master" GOTO RELEASE
-@echo SNAP
+@echo Publishing to snapshot repo
 ziti-ci publish artifactory --groupId=ziti-tunnel-win.amd64.windows --artifactId=ziti-tunnel-win --version=%ZITT_TUNNEL_WIN_VER%-SNAPSHOT --target=service/ziti-tunnel-win.zip
 GOTO END
 
 :RELEASE
-@echo REL
+@echo Publishing release
 ziti-ci publish artifactory --groupId=ziti-tunnel-win.amd64.windows --artifactId=ziti-tunnel-win --version=%ZITT_TUNNEL_WIN_VER% --target=service/ziti-tunnel-win.zip
 GOTO END
 
