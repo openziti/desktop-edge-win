bf369c968b372c9dbbde5b5adbdb5613236b3c37
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-05-25 11:07:01 -0400

    changed the variable name but missed a couple spots

--- service/publish.bat
+++ service/publish.bat
@@ -6,12 +6,12 @@ IF "%BUILD_VERSION%"=="" GOTO BUILD_VERSION_ERROR
 
 IF "%TRAVIS_BRANCH%"=="master" GOTO RELEASE
 @echo Publishing to snapshot repo
-ziti-ci publish artifactory --groupId=ziti-tunnel-win.amd64.windows --artifactId=ziti-tunnel-win --version=%ZITT_TUNNEL_WIN_VER%-SNAPSHOT --target=service/ziti-tunnel-win.zip
+ziti-ci publish artifactory --groupId=ziti-tunnel-win.amd64.windows --artifactId=ziti-tunnel-win --version=%BUILD_VERSION%-SNAPSHOT --target=service/ziti-tunnel-win.zip
 GOTO END
 
 :RELEASE
 @echo Publishing release
-ziti-ci publish artifactory --groupId=ziti-tunnel-win.amd64.windows --artifactId=ziti-tunnel-win --version=%ZITT_TUNNEL_WIN_VER% --target=service/ziti-tunnel-win.zip
+ziti-ci publish artifactory --groupId=ziti-tunnel-win.amd64.windows --artifactId=ziti-tunnel-win --version=%BUILD_VERSION% --target=service/ziti-tunnel-win.zip
 GOTO END
 
 :BUILD_VERSION_ERROR
