9f540fa4e0fdf18fba8918b55084d319686f8116
ziti-ci <ziti-ci@netfoundry.io>
2020-06-05 12:48:10 -0400

    Release 0.0.7

--- service/ziti-tunnel/version.go
+++ service/ziti-tunnel/version.go
@@ -4,8 +4,8 @@ package main
 
 const (
 	Version   = "0.0.7"
-	Revision  = "29e8293e9ec6"
+	Revision  = "d2189e59522a"
 	Branch    = "jun5"
 	BuildUser = "Clint D"
-	BuildDate = "2020-06-05 12:44:54"
+	BuildDate = "2020-06-05 12:48:10"
 )
