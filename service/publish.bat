354e4493ec17a641ddaf44868cb5eb1f5aaf168c
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-06-12 18:22:03 -0400

    travis still won't push

--- service/publish.bat
+++ service/publish.bat
@@ -32,22 +32,40 @@ GOTO END
 @echo The build version environment variable was not set - cannot publish
 exit /b 1
 
+:FAIL
+IF %~1 NEQ 0 (
+    echo ================================================================
+    echo.
+    echo FAILURE:
+    echo     %~2
+    echo.
+    echo ================================================================
+    exit /b %~1
+)
+exit /b 0
+
 :END
 echo configuring git - relies on build.bat successfully grabbing ziti-ci
 ziti-ci configure-git 2>1
 
 @echo publishing complete - committing version.go as ci
 
-@echo executing: git checkout %GIT_BRANCH%
-git checkout %GIT_BRANCH%
+git stash 2>1
+git checkout %GIT_BRANCH% 2>1
+git stash pop 2>1
+CALL :FAIL %ERRORLEVEL% "checkout failed"
+@echo git checkout %GIT_BRANCH% complete: %ERRORLEVEL%
 
 git add service/ziti-tunnel/version.go 2>1
+CALL :FAIL %ERRORLEVEL% "git add failed"
 @echo git add service/ziti-tunnel/version.go complete: %ERRORLEVEL%
 
 git commit -m "[ci skip] committing updated version information" 2>1
+CALL :FAIL %ERRORLEVEL% "git commit failed"
 @echo git commit -m "[ci skip] committing updated version information" complete: %ERRORLEVEL%
 
 git push 2>1
+CALL :FAIL %ERRORLEVEL% "git push failed"
 @echo git push complete: %ERRORLEVEL%
 
 cd %CURDIR%
