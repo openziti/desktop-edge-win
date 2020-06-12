ec7312a8d6ac4fb1ecd0ea2ea6ff635007ceb023
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-06-12 10:59:17 -0400

    quiet go get via > NUL

--- get-ziti-ci.bat
+++ get-ziti-ci.bat
@@ -3,12 +3,12 @@
 IF "x%ZITI_CI_VERSION%"=="x" GOTO DEFAULT
 
 echo Fetching ziti-ci@%ZITI_CI_VERSION%
-go get github.com/netfoundry/ziti-ci@%ZITI_CI_VERSION%
+go get github.com/netfoundry/ziti-ci@%ZITI_CI_VERSION% > NUL
 GOTO END
 
 :DEFAULT
 echo Fetching default ziti-ci
-go get github.com/netfoundry/ziti-ci
+go get github.com/netfoundry/ziti-ci > NUL
 GOTO END
 
 :END
