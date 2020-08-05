1a6a88ff803fcf639a4493bcb13f8884f5c4c73e
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-08-05 09:25:38 -0400

    remove old ssh-keyscan block

--- service/publish.bat
+++ service/publish.bat
@@ -69,10 +69,6 @@ git add service/ziti-tunnel/version.go 2>&1
 CALL :FAIL %ERRORLEVEL% "git add failed"
 @echo git add service/ziti-tunnel/version.go complete: %ERRORLEVEL%
 
-@echo issuing ssh -vT -i github_deploy_key git@github.com
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-ssh -vT -i github_deploy_key git@github.com 2>&1
-
 @echo issuing ssh-keyscan -t rsa github.com 2>&1 >> /root/.ssh/known_hosts
 ssh-keyscan -t rsa github.com 2>&1 >> /root/.ssh/known_hosts
 ssh -vT -i github_deploy_key git@github.com 2>&1
