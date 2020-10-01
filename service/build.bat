abc5de51dcbc79025b2500a92ac1490f62d2a692
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-10-01 16:33:03 -0400

    don't use master for tunnel sdk - use tag. better logging again

--- service/build.bat
+++ service/build.bat
@@ -34,7 +34,7 @@ echo changing to service folder: %SVC_ROOT_DIR%
 cd %SVC_ROOT_DIR%
 
 SET REPO_URL=https://github.com/openziti/ziti-tunneler-sdk-c.git
-SET ZITI_TUNNEL_REPO_BRANCH=master
+SET ZITI_TUNNEL_REPO_BRANCH=v0.6.7
 SET TUNNELER_SDK_DIR=%SVC_ROOT_DIR%deps\ziti-tunneler-sdk-c\
 SET CGO_CFLAGS=-DNOGDI -I %TUNNELER_SDK_DIR%install\include
 SET CGO_LDFLAGS=-L %TUNNELER_SDK_DIR%install\lib
