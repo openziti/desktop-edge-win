5f066458d63eca6c03f0bd61e0c31fa9db46c2eb
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-10-12 15:26:20 -0400

    adding a test-push.bat to see if we can get travis to push finall

---
+++ service/test-push.bat
@@ -0,0 +1,35 @@
+@echo off
+set CURDIR=%CD%
+set SVC_ROOT_DIR=%~dp0
+set ZITI_TUNNEL_WIN_ROOT=%SVC_ROOT_DIR%..\
+set /p BUILD_VERSION=<%ZITI_TUNNEL_WIN_ROOT%version
+set GO111MODULE=on
+cd /d %ZITI_TUNNEL_WIN_ROOT%
+
+echo fetching ziti-ci
+call %SVC_ROOT_DIR%/../get-ziti-ci.bat
+echo ziti-ci has been retrieved. running: ziti-ci version
+ziti-ci version
+
+echo generating version info - this will get pushed from publish.bat in CI _if_ publish.bat started build.bat
+ziti-ci generate-build-info --noAddNoCommit --useVersion=false %SVC_ROOT_DIR%/ziti-tunnel/version.go main --verbose 2>&1
+echo version info generated
+
+type version
+
+@echo ========================================================
+@echo trying git add and commit
+@echo ========================================================
+git add version
+git commit -m "updating version"
+
+
+@echo ========================================================
+@echo trying git push
+@echo ========================================================
+git push
+
+
+@echo ========================================================
+@echo all done
+@echo ========================================================
