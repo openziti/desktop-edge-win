a66eb6cfcceb4bd681d0c32d74ab9473ce8c05c5
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-10-12 16:13:48 -0400

    more git debugging on travis

--- service/test-push.bat
+++ service/test-push.bat
@@ -11,16 +11,24 @@ call %SVC_ROOT_DIR%/../get-ziti-ci.bat
 echo ziti-ci has been retrieved. running: ziti-ci version
 ziti-ci version
 
-echo generating version info - this will get pushed from publish.bat in CI _if_ publish.bat started build.bat
+@echo generating version info - this will get pushed from publish.bat in CI _if_ publish.bat started build.bat
 ziti-ci generate-build-info --noAddNoCommit --useVersion=false %SVC_ROOT_DIR%/ziti-tunnel/version.go main --verbose 2>&1
-echo version info generated
-
+@echo version info generated
+@echo ---------------------------
 type version
+@echo ---------------------------
 
 @echo ========================================================
 @echo trying git add and commit
 @echo ========================================================
+git diff
 git add service/ziti-tunnel/version.go
+@echo ---------------------------
+type service/ziti-tunnel/version.go
+@echo ---------------------------
+@echo ---------------------------
+type %SVC_ROOT_DIR%/ziti-tunnel/version.go
+@echo ---------------------------
 git diff
 git commit -m "updating version"
 git diff
@@ -29,6 +37,7 @@ git diff
 @echo trying git push
 @echo ========================================================
 git push
+git diff
 
 
 @echo ========================================================
