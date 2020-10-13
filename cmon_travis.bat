aefaa85964bb6c0890aedd4bff4fbcced9580651
ziti-ci <ziti-ci@netfoundry.io>
2020-10-13 14:21:56 -0400

    ok - surely this will work now right?

--- cmon_travis.bat
+++ cmon_travis.bat
@@ -1,9 +1,3 @@
-@echo looking for key using: ssh-keygen -F github.com __1__ 2>&1
-ssh-keygen -F github.com 2>&1
-
-@echo issuing ssh-keygen -R 2>&1
-ssh-keygen -R github.com 2>&1
-
 @echo mkdir and adding to actual known hosts... 2>&1
 @echo mkdir %USERPROFILE%\.ssh 2>&1
 mkdir %USERPROFILE%\.ssh 2>&1
@@ -11,11 +5,14 @@ mkdir %USERPROFILE%\.ssh 2>&1
 @echo adding github key: ssh-keyscan -t rsa github.com 2>&1
 ssh-keyscan -t rsa github.com >> %USERPROFILE%\.ssh\known_hosts 2>&1
 
-@echo typing %USERPROFILE%\.ssh\known_hosts 2>&1
-type %USERPROFILE%\.ssh\known_hosts 2>&1
-
 @echo looking for key using: ssh-keygen -F github.com - expect to find it this time! 2>&1
 ssh-keygen -F github.com 2>&1
+
+@echo "getting ziti-ci" 2>&1
+call get-ziti-ci.bat 2>&1
+@echo "ziti-ci has been retrieved. running\:ziti-ci version" 2>&1
+ziti-ci version 2>&1
+ziti-ci configure-git 2>&1
 
 @echo test ssh - expect it to work 2>&1
 ssh -Tv -i github_deploy_key git@github.com 2>&1
