2c9f06a1df50c892b545555b1b945602502aa430
ziti-ci <ziti-ci@netfoundry.io>
2020-10-13 14:01:11 -0400

    could this be it?

--- cmon_travis.bat
+++ cmon_travis.bat
@@ -18,7 +18,7 @@ type %USERPROFILE%\.ssh\known_hosts 2>&1
 ssh-keygen -F github.com 2>&1
 
 @echo test ssh - expect it to work 2>&1
-ssh -vT -i github_deploy_key github.com 2>&1
+ssh -Tv -i github_deploy_key git@github.com 2>&1
 
-@echo test ssh - manual known hosts 2>&1
-ssh -vT -o "UserKnownHostsFile manual-known-hosts.txt" -i github_deploy_key github.com 2>&1
+REM @echo test ssh - manual known hosts 2>&1
+REM ssh -vT -o "UserKnownHostsFile manual-known-hosts.txt" -i github_deploy_key github.com 2>&1
