ceb6a247bdae98d2592ae5cdef4036c8dee616a2
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-10-12 21:18:49 -0400

    still trying to get travis to not block forever

--- service/test-push.bat
+++ service/test-push.bat
@@ -14,28 +14,11 @@ ziti-ci version 2>&1
 @echo generating version info - this will get pushed from publish.bat in CI _if_ publish.bat started build.bat 2>&1
 ziti-ci generate-build-info --noAddNoCommit --useVersion=false %SVC_ROOT_DIR%/ziti-tunnel/version.go main --verbose 2>&1
 @echo version info generated 2>&1
-@echo --------------------------- 2>&1
-type version
-@echo --------------------------- 2>&1
 
-@echo ======================================================== 2>&1
-@echo trying git add and commit 2>&1
-@echo ======================================================== 2>&1
-git diff 2>&1
 git add service/ziti-tunnel/version.go 2>&1
-
-
-@echo issuing git diff 2>&1
-git diff 2>&1
 
 @echo issuing git commit 2>&1
 git commit -m "[ci skip] updating version" 2>&1
-
-@echo issuing git diff 2>&1
-git diff 2>&1
-
-@echo issuing git status 2>&1
-git status 2>&1
 
 @echo issuing git push origin HEAD:%GIT_BRANCH% 2>&1
 
