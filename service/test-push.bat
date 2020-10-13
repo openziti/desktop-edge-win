d4a8d7fa4b9acb111921a178ac6e1742a7251a15
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-10-12 23:03:09 -0400

    remove invalid commands

--- service/test-push.bat
+++ service/test-push.bat
@@ -6,15 +6,10 @@ set /p BUILD_VERSION=<%ZITI_TUNNEL_WIN_ROOT%version
 set GO111MODULE=on
 cd /d %ZITI_TUNNEL_WIN_ROOT%
 
-
-
 @echo converting shallow clone so travis can co: %GIT_BRANCH%
 git remote set-branches origin %GIT_BRANCH% 2>&1
 git fetch --depth 1 origin %GIT_BRANCH% 2>&1
 git checkout %GIT_BRANCH% 2>&1
-
-
-
 
 echo fetching ziti-ci 2>&1
 call %SVC_ROOT_DIR%/../get-ziti-ci.bat
@@ -33,25 +28,13 @@ type version 2>&1
 @echo ======================================================== 2>&1
 
 git add service/ziti-tunnel/version.go 2>&1
-@echo --------------------------- 2>&1
-type service/ziti-tunnel/version.go 2>&1
-@echo --------------------------- 2>&1
-@echo --------------------------- 2>&1
-type %SVC_ROOT_DIR%/ziti-tunnel/version.go 2>&1
-@echo --------------------------- 2>&1
-
 git commit -m "[skip ci] updating version" 2>&1
-
 
 @echo ======================================================== 2>&1
 @echo trying git push 2>&1
 @echo ======================================================== 2>&1
 git status 2>&1
 git push 2>&1
-rem origin HEAD:%GIT_BRANCH% 2>&1
-REM git push origin HEAD:<name-of-remote-branch> 2>&1
-REM git diff 2>&1
-
 
 @echo ======================================================== 2>&1
 @echo all done 2>&1
