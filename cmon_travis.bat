6f31b39cbe238b58fe5818968cb32ed80f4e2ba5
ziti-ci <ziti-ci@netfoundry.io>
2020-10-13 13:04:16 -0400

    another update........

--- cmon_travis.bat
+++ cmon_travis.bat
@@ -1,10 +1,11 @@
-echo "about to try to echo a key onto known hosts..."
-echo "nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8" >> %USERPROFILE%/.ssh/known_hosts
-type %USERPROFILE%/.ssh/known_hosts
-echo "getting ziti-ci"
-call get-ziti-ci.bat
-echo "ziti-ci has been retrieved. running\:ziti-ci version"
-ziti-ci version
-ziti-ci configure-git
-C:\Program Files\OpenSSH-Win64\ssh.exe -vT -i github_deploy_key github.com
-ssh -vT -i github_deploy_key github.com
+@echo off
+@echo "about to try to echo a key onto known hosts..." 2>&1
+@echo "nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8" >> %USERPROFILE%\.ssh\known_hosts
+type %USERPROFILE%\.ssh\known_hosts 2>&1
+@echo "getting ziti-ci" 2>&1
+call get-ziti-ci.bat 2>&1
+@echo "ziti-ci has been retrieved. running\:ziti-ci version" 2>&1
+ziti-ci version 2>&1
+ziti-ci configure-git 2>&1
+"C:\Program Files\OpenSSH-Win64\ssh.exe" -vT -i github_deploy_key github.com 2>&1
+ssh -vT -i github_deploy_key github.com 2>&1
