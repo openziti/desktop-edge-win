56ec6ecceeb6b87209650d30f86b6e960d2c8e99
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-10-19 23:35:48 -0400

    don't append newlines over and over

--- update-versions.ps1
+++ update-versions.ps1
@@ -4,8 +4,8 @@ echo "          version: $v"
 echo ""
 $assemblyInfo="./DesktopEdge/Properties/AssemblyInfo.cs"
 echo "Replacing version in $assemblyInfo"
-(Get-Content -Encoding UTF8 -path $assemblyInfo -Raw) -replace '\d\.\d\.\d\.', "$v." | Set-Content -Path $assemblyInfo -Encoding UTF8
+(Get-Content -Encoding UTF8 -path $assemblyInfo -Raw) -replace '\d\.\d\.\d\.', "$v." | Set-Content -Path $assemblyInfo -Encoding UTF8 -NoNewline
 
 $installer="./Installer/ZitiDesktopEdge.aip"
 echo "Replacing version in $installer"
-(Get-Content -Encoding UTF8 -path $installer -Raw) -replace '"ProductVersion" Value="\d\.\d\.\d"', """ProductVersion"" Value=""${v}""" | Set-Content -Path $installer -Encoding UTF8
+(Get-Content -Encoding UTF8 -path $installer -Raw) -replace '"ProductVersion" Value="\d\.\d\.\d"', """ProductVersion"" Value=""${v}""" | Set-Content -Path $installer -Encoding UTF8 -NoNewline
