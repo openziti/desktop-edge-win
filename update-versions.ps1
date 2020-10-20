258e4b5c6f3411564b8217b25cda88813c9ac767
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-10-20 10:24:01 -0400

    update travis to get versions updated automatically if needed

--- update-versions.ps1
+++ update-versions.ps1
@@ -1,3 +1,4 @@
+echo "==================================== update-versions.ps1 begins ===================================="
 echo "Obtaining version information from .\version"
 $v=(Get-Content -Path .\version)
 echo "          version: $v"
@@ -13,11 +14,28 @@ $installerReplaced="${installer}.replaced"
 
 echo "Replacing version in $assemblyInfo into $assemblyInfoReplaced"
 (Get-Content -Encoding UTF8 -path $assemblyInfo -Raw) -replace 'Version\("[0-9]*.[0-9]*.[0-9]*.0', "Version\(""${v}.0" | Set-Content -Encoding UTF8 -Path "$assemblyInfoReplaced" -NoNewline
+
+echo "echoing content of $assemblyInfoReplaced"
+(Get-Content -Encoding UTF8 -path $assemblyInfoReplaced -Raw)
+
 rm $assemblyInfo
 mv $assemblyInfoReplaced $assemblyInfo
 
+echo "echoing content of $assemblyInfo"
+(Get-Content -Encoding UTF8 -path $assemblyInfo -Raw)
+
 echo "Replacing version in $installer into $installerReplaced"
 (Get-Content -Encoding UTF8 -path $installer -Raw) -replace '"ProductVersion" Value="[0-9]*\.[0-9]*\.[0-9]*"', """ProductVersion"" Value=""${v}""" | Set-Content -Path "${installer}.replaced" -Encoding UTF8 -NoNewline
+
+echo "echoing content of $installerReplaced"
+(Get-Content -Encoding UTF8 -path $installerReplaced -Raw)
+
 rm $installer
 mv $installerReplaced $installer
 
+echo "echoing content of $installer"
+(Get-Content -Encoding UTF8 -path $installer -Raw)
+
+git status
+
+echo "==================================== update-versions.ps1 complete ===================================="
