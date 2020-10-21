ba170b915a1098495b15bd8ee8959092a3fc2a5e
ziti-ci <ziti-ci@netfoundry.io>
2020-10-21 04:55:43 +0000

    [ci skip] - [***NO_CI***] [skip ci] committing updated version information

--- service/ziti-tunnel/version.go
+++ service/ziti-tunnel/version.go
@@ -21,8 +21,8 @@ package main
 
 const (
 	Version   = "1.2.11"
-	Revision  = "bef353f8d097"
+	Revision  = "86fb8edfc357"
 	Branch    = "master"
-	BuildUser = "travis"
-	BuildDate = "2020-10-21 04:44:26"
+	BuildUser = ""
+	BuildDate = "2020-10-21 04:52:08"
 )
