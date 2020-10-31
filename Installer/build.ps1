130a8b55e7b52b113485160d5f8d2d3d508cba64
ziti-ci <ziti-ci@netfoundry.io>
2020-10-31 14:00:45 -0400

    update to build scripts

--- Installer/build.ps1
+++ Installer/build.ps1
@@ -4,47 +4,45 @@ rm .\build -r -fo -ErrorAction Ignore
 
 $invocation = (Get-Variable MyInvocation).Value
 $scriptPath = Split-Path $invocation.MyCommand.Path
-${scriptPath}
-
-$x=[xml] @"
-$((Invoke-WebRequest https://netfoundry.jfrog.io/artifactory/ziti-maven-snapshot/ziti-tunnel-win/amd64/windows/ziti-tunnel-win/maven-metadata.xml).Content)
-"@
-
+#${scriptPath}
+#
+#$x=[xml] @"
+#$((Invoke-WebRequest https://netfoundry.jfrog.io/artifactory/ziti-maven-snapshot/ziti-tunnel-win/amd64/windows/ziti-tunnel-win/maven-metadata.xml).Content)
+#"@
+#
 echo "the branch is $env:GIT_BRANCH"
-
-$branch = git rev-parse --abbrev-ref HEAD
-
-if ("$branch" -ne 'master') {
-    #if the git command to get the branch fails - this is 'not master' use the 'latest' - else fetch the 'release'
-    $serviceVersion = $x.metadata.versioning.latest
-}
-else {
-    $serviceVersion = $x.metadata.versioning.release
-}
-echo "service version is: $serviceVersion"
-$installerVersion=(Get-Content -Path .\version)
-
-$zipUrl = "https://netfoundry.jfrog.io/artifactory/ziti-maven-snapshot/ziti-tunnel-win/amd64/windows/ziti-tunnel-win/${serviceVersion}/ziti-tunnel-win-${serviceVersion}.zip"
-
-echo "Downloading zip file "
-echo "      from: $zipUrl"
-
-tree ${scriptPath}..
-
-$ProgressPreference = 'SilentlyContinue'
-$timeTaken = Measure-Command -Expression {
-    Invoke-WebRequest $zipUrl -OutFile ziti-tunnel-service.zip
-}
+#
+#$branch = git rev-parse --abbrev-ref HEAD
+#
+#if ("$branch" -ne 'master') {
+#    #if the git command to get the branch fails - this is 'not master' use the 'latest' - else fetch the 'release'
+#    $serviceVersion = $x.metadata.versioning.latest
+#}
+#else {
+#    $serviceVersion = $x.metadata.versioning.release
+#}
+#echo "service version is: $serviceVersion"
+#
+#$zipUrl = "https://netfoundry.jfrog.io/artifactory/ziti-maven-snapshot/ziti-tunnel-win/amd64/windows/ziti-tunnel-win/${serviceVersion}/ziti-tunnel-win-${serviceVersion}.zip"
+#
+#echo "Downloading zip file "
+#echo "      from: $zipUrl"
+#
+#tree ${scriptPath}..
+#
+#$ProgressPreference = 'SilentlyContinue'
+#$timeTaken = Measure-Command -Expression {
+#    Invoke-WebRequest $zipUrl -OutFile ziti-tunnel-service.zip
+#}
+#$milliseconds = $timeTaken.TotalMilliseconds
+#echo "      time to download: $milliseconds"
+#$ProgressPreference = 'Continue'
 
 $zipLocal = "ziti-tunnel-service.zip"
 $zipLocal = "${scriptPath}/../service/ziti-tunnel-win.zip"
 
-$milliseconds = $timeTaken.TotalMilliseconds
-echo "      time to download: $milliseconds"
 echo ""
 echo "unzipping $zipLocal to ${scriptPath}\build\service\"
-
-$ProgressPreference = 'Continue'
 
 Expand-Archive -Verbose -Force -LiteralPath $zipLocal "${scriptPath}\build\service\"
 
@@ -63,8 +61,9 @@ Pop-Location
 
 $ADVINST = "C:\Program Files (x86)\Caphyon\Advanced Installer 17.6\bin\x86\AdvancedInstaller.com"
 $ADVPROJECT = "${scriptPath}\ZitiDesktopEdge.aip"
-
+$installerVersion=(Get-Content -Path ${scriptPath}\..\version)
 $action = '/SetVersion'
+
 echo "issuing $ADVINST /edit $ADVPROJECT $action $installerVersion (service version: $serviceVersion) - see https://www.advancedinstaller.com/user-guide/set-version.html"
 & $ADVINST /edit $ADVPROJECT $action $installerVersion
 
