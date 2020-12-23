a4b8f4528ab01a929b991d81b3134c66a0647d07
ziti-ci <ziti-ci@netfoundry.io>
2020-12-22 20:22:03 -0500

    update reg.bat to 17.8 again

--- Installer/reg.bat
+++ Installer/reg.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Caphyon\Advanced Installer 17.7\bin\x86\AdvancedInstaller.com" /register %1
+"C:\Program Files (x86)\Caphyon\Advanced Installer 17.8\bin\x86\AdvancedInstaller.com" /register %1
