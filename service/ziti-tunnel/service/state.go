820d138e29307bf63b024e6d987d0faff0320490
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2021-02-23 20:34:38 -0500

    missed a merge

--- service/ziti-tunnel/service/state.go
+++ service/ziti-tunnel/service/state.go
@@ -267,7 +267,7 @@ func (t *RuntimeState) LoadIdentity(id *Id, refreshInterval int) {
 		}
 
 		events.broadcast <- dto.IdentityEvent{
-			ActionEvent: IDENTITY_ADDED,
+			ActionEvent: dto.IDENTITY_ADDED,
 			Id:          id.Identity,
 		}
 		log.Infof("connecting identity completed: %s[%s]", id.Name, id.FingerPrint)
