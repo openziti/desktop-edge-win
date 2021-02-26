fa75c2df12efcb2bffc6f0f6926e08296c024fe3
Mary Lincy Dcouto <mary.dcouto@netfoundry.io>
2021-02-26 22:20:07 +0530

    use string literals instead of string objects

--- service/ziti-tunnel/cli/cmdlineConstants.go
+++ service/ziti-tunnel/cli/cmdlineConstants.go
@@ -26,10 +26,12 @@ var GET_STATUS = dto.CommandMsg{
 	Function: "Status",
 }
 
-var templateIdentity = "{{printf \"%40s\" \"Name\"}} | {{printf \"%41s\" \"FingerPrint\"}} | {{printf \"%6s\" \"Active\"}} | {{printf \"%30s\" \"Config\"}} | {{\"Status\"}}\n" +
-	"{{range .}}{{printf \"%40s\" .Name}} | {{printf \"%41s\" .FingerPrint}} | {{printf \"%6t\" .Active}} | {{printf \"%30s\" .Config}} | {{.Status}}\n{{end}}"
+var templateIdentity = `{{printf "%40s" "Name"}} | {{printf "%41s" "FingerPrint"}} | {{printf "%6s" "Active"}} | {{printf "%30s" "Config"}} | {{"Status"}}
+{{range .}}{{printf "%40s" .Name}} | {{printf "%41s" .FingerPrint}} | {{printf "%6t" .Active}} | {{printf "%30s" .Config}} | {{.Status}}
+{{end}}`
 
-var templateService = "{{printf \"%40s\" \"Name\"}} | {{printf \"%15s\" \"Id\"}} | {{printf \"%40s\" \"InterceptHost\"}} | {{printf \"%14s\" \"InterceptPort\"}} | {{printf \"%15s\" \"AssignedIP\"}} | {{printf \"%15s\" \"AssignedHost\"}} | {{\"OwnsIntercept\"}}\n" +
-	"{{range .}}{{printf \"%40s\" .Name}} | {{printf \"%15s\" .Id}} | {{printf \"%40s\" .InterceptHost}} | {{printf \"%14d\" .InterceptPort}} | {{printf \"%15s\" .AssignedIP}} | {{printf \"%15s\" .AssignedHost}} | {{.OwnsIntercept}}\n{{end}}"
+var templateService = `{{printf "%40s" "Name"}} | {{printf "%15s" "Id"}} | {{printf "%40s" "InterceptHost"}} | {{printf "%14s" "InterceptPort"}} | {{printf "%15s" "AssignedIP"}} | {{printf "%15s" "AssignedHost"}} | {{"OwnsIntercept"}}
+{{range .}}{{printf "%40s" .Name}} | {{printf "%15s" .Id}} | {{printf "%40s" .InterceptHost}} | {{printf "%14d" .InterceptPort}} | {{printf "%15s" .AssignedIP}} | {{printf "%15s" .AssignedHost}} | {{.OwnsIntercept}}
+{{end}}`
 
 var log = logging.Logger()
--- service/ziti-tunnel/cmd/services.go
+++ service/ziti-tunnel/cmd/services.go
@@ -26,12 +26,8 @@ import (
 var servicesCmd = &cobra.Command{
 	Use:   "services",
 	Short: "Lists services from ziti-tunnel",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `View the services that this user has access to.
+	The records will be fetched from ziti-tunnel`,
 	Run: func(cmd *cobra.Command, args []string) {
 		flags := map[string]bool{}
 		flags["prettyJSON"] = prettyJSON
