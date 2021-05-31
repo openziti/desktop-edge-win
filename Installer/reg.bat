ed9a214d5c3359e73fc31cb65824d054f7ec2533
Mary Lincy Dcouto <mary.dcouto@netfoundry.io>
2021-05-31 20:02:46 +0530

    update Advanced Installer version

--- Installer/reg.bat
+++ Installer/reg.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Caphyon\Advanced Installer 18.2\bin\x86\AdvancedInstaller.com" /register %1
+"C:\Program Files (x86)\Caphyon\Advanced Installer 18.3\bin\x86\AdvancedInstaller.com" /register %1
