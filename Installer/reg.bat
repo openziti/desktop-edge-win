ff681fd2f0ea5a018c7af6678cb5eaa92435957c
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2022-07-18 13:53:13 -0400

    19.7 not pushed to choco yet...

--- Installer/reg.bat
+++ Installer/reg.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Caphyon\Advanced Installer 19.7\bin\x86\AdvancedInstaller.com" /register %1
+"C:\Program Files (x86)\Caphyon\Advanced Installer 19.6\bin\x86\AdvancedInstaller.com" /register %1
