0029f29eb87461eaf9e58fd78f952278a8ccb4f5
Shawn Carey <shawn.carey@netfoundry.io>
2022-10-17 09:41:26 -0400

    update advanced installer to 0.19.9

--- Installer/reg.bat
+++ Installer/reg.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Caphyon\Advanced Installer 19.8.1\bin\x86\AdvancedInstaller.com" /register %1
+"C:\Program Files (x86)\Caphyon\Advanced Installer 19.9\bin\x86\AdvancedInstaller.com" /register %1
