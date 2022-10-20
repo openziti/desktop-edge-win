26b16c36affeb23b33ce6ddd22e7cf01addd568f
Shawn Carey <shawn.carey@netfoundry.io>
2022-10-20 10:54:14 -0400

    update tsdk to 0.20.6

--- ziti-edge-tunnel/build.bat
+++ ziti-edge-tunnel/build.bat
@@ -14,7 +14,7 @@ REM See the License for the specific language governing permissions and
 REM limitations under the License.
 REM
 SET REPO_URL=https://github.com/openziti/ziti-tunnel-sdk-c.git
-SET ZITI_TUNNEL_REPO_BRANCH=v0.20.4
+SET ZITI_TUNNEL_REPO_BRANCH=v0.20.6
 REM override the c sdk used in the build - leave blank for the same as specified in the tunneler sdk
 SET ZITI_SDK_C_BRANCH=
 SET ZITI_TUNNEL_REPO_URL=https://github.com/openziti/ziti-tunnel-sdk-c/releases/latest/download/ziti-edge-tunnel-Windows_x86_64.zip
