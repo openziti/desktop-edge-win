f90ce618b4c8b4a31bded2180f9544c736b84d1d
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-02-06 14:53:54 -0500

    build with new advanced installer where internet connection is not req'd

--- Installer/reg.bat
+++ Installer/reg.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Caphyon\Advanced Installer 20.2\bin\x86\AdvancedInstaller.com" /register %1
+"C:\Program Files (x86)\Caphyon\Advanced Installer 20.3\bin\x86\AdvancedInstaller.com" /register %1
