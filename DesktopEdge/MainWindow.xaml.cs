0064318938ad0f2f0ae2209f178cf87c932c11fb
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-08-15 17:49:19 -0400

    more NetFoundry, -> NetFoundry

--- DesktopEdge/MainWindow.xaml.cs
+++ DesktopEdge/MainWindow.xaml.cs
@@ -69,7 +69,7 @@ namespace ZitiDesktopEdge {
 			ThisAssemblyName = asm.GetName().Name;
 			state = (ZDEWViewState)Application.Current.Properties["ZDEWViewState"];
 #if DEBUG
-			ExecutionDirectory = @"C:\Program Files (x86)\NetFoundry, Inc\Ziti Desktop Edge";
+			ExecutionDirectory = @"C:\Program Files (x86)\NetFoundry Inc\Ziti Desktop Edge";
 #else
 			ExecutionDirectory = Path.GetDirectoryName(asm.Location);
 #endif
