58ebc4182f94602bdd4c5abe56027021bb3d0685
Shawn Carey <shawn.carey@netfoundry.io>
2023-08-16 17:00:50 +0000

    prepare for release 2.2.17 (tsdk 0.22.1) (#608)
    update tsdk to 0.22.1/0.33.2

--- Installer/reg.bat
+++ Installer/reg.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Caphyon\Advanced Installer 20.4.1\bin\x86\AdvancedInstaller.com" /register %1
+"C:\Program Files (x86)\Caphyon\Advanced Installer 20.9.1\bin\x86\AdvancedInstaller.com" /register %1
