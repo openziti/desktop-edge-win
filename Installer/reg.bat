7ee6c258bbe7c3f554b62c3e1f49832413485483
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-11-10 16:07:48 -0500

    fix merge issue

--- Installer/reg.bat
+++ Installer/reg.bat
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
 "C:\Program Files (x86)\Caphyon\Advanced Installer 21.2.1\bin\x86\AdvancedInstaller.com" /register %1
-=======
-"C:\Program Files (x86)\Caphyon\Advanced Installer 21.2.1\bin\x86\AdvancedInstaller.com" /register %1
->>>>>>> Stashed changes
