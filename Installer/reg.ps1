df90994e33c92a4662326be05b40bd700810a884
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-12-20 17:55:13 -0500

    i do so love debugging github actions

--- Installer/reg.ps1
+++ Installer/reg.ps1
@@ -1,4 +1,4 @@
 param(
     [string]$key
 )
-&('C:\Program Files (x86)\Caphyon\Advanced Installer 21.3\bin\x86\AdvancedInstaller.com') /register $key
+&("C:\Program Files (x86)\Caphyon\Advanced Installer ${env:ADV_INST_VERSION}\bin\x86\AdvancedInstaller.com") /register $key
