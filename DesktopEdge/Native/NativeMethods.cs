0eccd8a58c31bdd00c27263626840f4bddcef7f2
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-03-01 14:26:35 -0500

    merge release-next to main and produce 2.2.1.7 (#633)
    * trying to add header file to all .cs files
    
    * revert the logo back to original
    
    * cleanup icon use
    
    * attempt to placate adv installer
    
    * add generated imgs from adv installer too...
    
    * update images from installer again
    
    * updates to readmes
    
    * update sln too
    
    * update latest to 2.2.1.6
    
    * updates to advinst
    
    * use the proper version
    
    * add version file to the .sln
    
    * minor tweaks to build script when using local built stuffs
    
    * push adv inst versoin
    
    * fix adv installer registration
    
    * use &() to execute the string
    
    * no need to write it to a file
    
    * too many hyphens
    
    * i do so love debugging github actions
    
    * remove local-build and ziti-edge-tunnel build and wintun.zip (#641)
    
    * update tsdk/csdk to 0.22.23 / 0.36.6 (#642)
    
    * update tsdk/csdk to 0.22.23 / 0.36.6
    * use ZITI_EDGE_TUNNEL_VERSION if env:ZITI_EDGE_TUNNEL_BUILD is not set
    
    * get tsdk 0.22.24 (#643)
    
    * get tsdk 0.22.24
    
    * update advanced installer
    
    * set version to 2.2.2 (#644)
    
    ---------
    
    Co-authored-by: Shawn Carey <shawn.carey@netfoundry.io>

--- DesktopEdge/Native/NativeMethods.cs
+++ DesktopEdge/Native/NativeMethods.cs
@@ -1,1 +1,17 @@
+/*
+	Copyright NetFoundry Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
 ﻿using System;using System.Collections.Generic;using System.ComponentModel;using System.IO;using System.Linq;using System.Runtime.InteropServices;using System.Text;using System.Threading.Tasks;namespace ZitiDesktopEdge.Native {    public static class NativeMethods {        private static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);        private const uint FILE_READ_EA = 0x0008;        private const uint FILE_FLAG_BACKUP_SEMANTICS = 0x2000000;        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]        static extern uint GetFinalPathNameByHandle(IntPtr hFile, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpszFilePath, uint cchFilePath, uint dwFlags);        [DllImport("kernel32.dll", SetLastError = true)]        [return: MarshalAs(UnmanagedType.Bool)]        static extern bool CloseHandle(IntPtr hObject);        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]        public static extern IntPtr CreateFile(                [MarshalAs(UnmanagedType.LPTStr)] string filename,                [MarshalAs(UnmanagedType.U4)] uint access,                [MarshalAs(UnmanagedType.U4)] FileShare share,                IntPtr securityAttributes, // optional SECURITY_ATTRIBUTES struct or IntPtr.Zero                [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,                [MarshalAs(UnmanagedType.U4)] uint flagsAndAttributes,                IntPtr templateFile);        public static string GetFinalPathName(string path) {            var h = CreateFile(path,                FILE_READ_EA,                FileShare.ReadWrite | FileShare.Delete,                IntPtr.Zero,                FileMode.Open,                FILE_FLAG_BACKUP_SEMANTICS,                IntPtr.Zero);            if (h == INVALID_HANDLE_VALUE)                throw new Win32Exception();            try {                var sb = new StringBuilder(1024);                var res = GetFinalPathNameByHandle(h, sb, 1024, 0);                if (res == 0)                    throw new Win32Exception();                return sb.ToString();            } finally {                CloseHandle(h);            }        }    }}
---
+++ Installer/reg.ps1
@@ -0,0 +1,4 @@
+param(
+    [string]$key
+)
+&("C:\Program Files (x86)\Caphyon\Advanced Installer ${env:ADV_INST_VERSION}\bin\x86\AdvancedInstaller.com") /register $key
